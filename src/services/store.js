70f19f1f6f199e17047580ea3cb8e025601c51dc
priyanshunegi04 <priyanshunegi966@gmail.com>
2023-08-05 16:55:45 +0530

    first commit

---
+++ src/services/store.js
@@ -0,0 +1,13 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+
+import { articleApi } from "./article";
+
+
+
+
+export const store = configureStore({
+reducer:{
+[articleApi.reducerPath]: articleApi.reducer
+},
+middleware: (getDefaultMiddleware) =>getDefaultMiddleware().concat(articleApi.middleware)
+});
